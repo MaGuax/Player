26c973d10111268236a6410459a3b9d32e8009a8
AIERXUAN\aierxuan <574277409@qq.com>
2017-08-11 17:27:17 +0800

    no message

--- js/maguaplayer.js
+++ js/maguaplayer.js
@@ -1,4 +1,3 @@
-////辅助函数
 //找出旧下标
 var oldindex = (name, list) => {
     for(var i = 0; i < list.length; i++) {
@@ -8,7 +7,6 @@ var oldindex = (name, list) => {
         }
     }
 }
-
 //歌曲路径加载成功时播放
 var ok = () => {
     var a = e("#id-audio-player")
@@ -18,7 +16,7 @@ var ok = () => {
         a.play()
     })
 }
-
+//实时改变进度条
 var changeCss = (selector, key, value) => {
       selector.style[key] = value
 }
@@ -36,31 +34,17 @@ var changeSong = (path, list, offset) => {
     var newIndex = (number + nowIndex + offset) % number
     return list[newIndex]
 }
-
-// 上一首按钮
-var up = () => {
-    var s = e("#id-button-last")
+//切歌
+var cutSong = () => {
+    var b = es('.cut')
     var control = e("#id-button-control")
-    bindEvent(s, 'click', function() {
+    bindAll(b, 'click', function() {
+        var k = event.target
         toggleClass(control, 'play')
         var a = e('#id-audio-player')
         var nowPath = a.dataset.path
-        var nextPath = "mp3/" + changeSong(nowPath, songs, -1)
-        a.src = nextPath
-        a.dataset.path= nextPath
-        a.play()
-    })
-}
-
-// 下一首按钮
-var down = () => {
-    var s = e("#id-button-next")
-    var control = e("#id-button-control")
-    bindEvent(s, 'click', function() {
-        toggleClass(control, 'play')
-        var a = e('#id-audio-player')
-        var nowPath = a.dataset.path
-        var nextPath = "mp3/" + changeSong(nowPath, songs, 1)
+        var number = Number(k.dataset.n)
+        var nextPath = "mp3/" + changeSong(nowPath, songs, number)
         a.src = nextPath
         a.dataset.path= nextPath
         a.play()
@@ -101,7 +85,6 @@ var click = function() {
         }
     })
 }
-// 控制按钮
 var control = () => {
     style()
     click()
@@ -109,14 +92,11 @@ var control = () => {
 
 //播放循序列表
 var stateList = ["inturn", "randomPlay", "one"]
-
 //单曲
 var one = function() {
-    log('one')
     var a = e("#id-audio-player")
     a.play()
 }
-
 //循序
 var ranking = 0
 var nextSong = function() {
@@ -127,10 +107,7 @@ var inturn = function() {
     var a = e('#id-audio-player')
     var song = 'mp3/' + nextSong()
     a.src = song
-    log('inturnsong', song)
-    //a.play()
 }
-
 //随机
 var random01 = function() {
     var n = Math.random()
@@ -153,11 +130,9 @@ var randomPlay = function() {
     var a = e('#id-audio-player')
     var song = randomSong()
     a.src = song
-    log('randomsong', song)
-    //a.play()
 }
 
-//新播放循序下标
+//新播放顺序下标
 var changeState = (old, list) => {
     var number = list.length
     var name = list[old]
@@ -165,21 +140,27 @@ var changeState = (old, list) => {
     var newIndex = (number + nowIndex + 1) % number
     return newIndex
 }
+//播放循序
+var oder = {
+    '0': '循环',
+    '1': '随机',
+    '2': '单曲',
+}
 //改变播放循序
 var stateOder = () => {
     var s = e("#id-button-oder")
     var oldState = s.dataset.state
     var newState = changeState(oldState, stateList)
     s.dataset.state = newState
-    if (newState == '0') {
-        s.innerHTML = '循环'
-    }else if (newState == '1') {
-        s.innerHTML = '随机'
-    }else if (newState == '2'){
-        s.innerHTML = '单曲'
-    }
+    s.innerHTML = oder[newState]
 }
-
+//点击切换播放模式按钮
+var oderChlick = () => {
+    var s = e("#id-button-oder")
+    bindEvent(s, "click", function() {
+        stateOder()
+    })
+}
 //根据播放模式决定下一首歌
 var oderChioce = (key) => {
     var s = e('#id-button-oder')
@@ -191,14 +172,6 @@ var oderChioce = (key) => {
         one()
     }
 }
-//点击切换播放模式按钮
-var oderChlick = () => {
-    var s = e("#id-button-oder")
-    bindEvent(s, "click", function() {
-        stateOder()
-    })
-}
-
 //// 结束时根据播放模式继续播放
 var oderPlay = () => {
     var a = e('#id-audio-player')
@@ -209,10 +182,7 @@ var oderPlay = () => {
         oderChioce(oderIndex)
         ok()
     })
-
 }
-
-
 ////点击歌曲列表切换歌曲
 var changeMusic = function() {
     var selector = es('.music')
@@ -225,7 +195,6 @@ var changeMusic = function() {
         ok()
     })
 }
-
 ////进度条
 var bar = () => {
     var a = e('#id-audio-player')
@@ -239,27 +208,13 @@ var bar = () => {
     })
 }
 
-//音量控制
-var voice = () => {
-    var v = e('#id-input-voice')
-    var a = e('#id-audio-player')
-    a.volume = Number(v.value * 0.01)
-    bindEvent(v, 'change', function() {
-        var value = (v.value) * 0.01
-        log(value)
-        a.volume = Number(value)
-    })
-}
-
 var __main = () => {
-    up()
-    down()
+    cutSong()
     control()
     oderChlick()
     oderPlay()
     changeMusic()
     bar()
-    //voice()
 }
 
 __main()
